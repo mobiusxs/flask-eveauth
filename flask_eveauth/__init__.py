27b743a9dc07fe402fe20db01d7ad0a09796c63c
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-08-07 07:56:28 -0500

    make decorators immediately importable

--- flask_eveauth/__init__.py
+++ flask_eveauth/__init__.py
@@ -1,1 +1,5 @@
 from .auth import Auth
+from .decorators import authentication_prohibited
+from .decorators import authentication_required
+from .decorators import role_required
+from .utils import get_current_user
