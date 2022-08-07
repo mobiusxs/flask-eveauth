4fcc6ebe7dffecee56f93f5723ffdd471cc4825c
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-08-07 06:55:01 -0500

    create anonymous user

--- flask_eveauth/utils.py
+++ flask_eveauth/utils.py
@@ -1,2 +1,14 @@
+from .models import User
+
+anonymous_session = User(
+    expires_in=-1,
+    token_type='anonymous',
+    refresh_token='',
+    access_token='',
+    name='Anonymous User',
+    character_id=1
+)
+
+
 def get_current_user():
     pass
